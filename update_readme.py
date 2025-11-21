6cd283df46635ba9da4e48dcb34f56bd10512768
LTM0714 <80049651+LTM0714@users.noreply.github.com>
2025-11-21 20:47:46 +0900

    Remove fetching latest commit from README update

--- update_readme.py
+++ update_readme.py
@@ -8,13 +8,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 content = content.replace("{{TIME}}", now)
 
-# 2. 최근 커밋
-user = "LTM0714"
-repo = "LTM0714"
-commits = requests.get(f"https://api.github.com/repos/{user}/{repo}/commits").json()
-latest_commit = commits[0]["commit"]["message"]
-content = content.replace("{{COMMIT}}", latest_commit)
-
 # 저장
 with open("README.md", "w", encoding="utf-8") as f:
+
     f.write(content)
