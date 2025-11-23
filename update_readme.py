bdf3c2a6c28f1bbe2abcd166da36db2c1ceb0f05
LTM0714 <80049651+LTM0714@users.noreply.github.com>
2025-11-23 22:17:53 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,15 +1,18 @@
 from datetime import datetime
+import re
 
 README_PATH = "README.md"
-
 now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 with open(README_PATH, "r", encoding="utf-8") as file:
     content = file.read()
 
-content = content.replace("{{TIME}}", now)
+# "현재 시간:" 뒤의 모든 내용을 최신 시간으로 교체
+content = re.sub(
+    r"🕒 현재 시간:.*",
+    f"🕒 현재 시간: {now}",
+    content
+)
 
 with open(README_PATH, "w", encoding="utf-8") as file:
     file.write(content)
-
-
